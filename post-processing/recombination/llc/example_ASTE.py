e74e196e74515d66a2498f96affd9ad23e923d90
Raphael Dussin <raphael.dussin@gmail.com>
2018-05-09 16:51:42 -0500

    add 4d read and filealt

--- post-processing/recombination/llc/example_ASTE.py
+++ post-processing/recombination/llc/example_ASTE.py
@@ -3,7 +3,17 @@ from matplotlib import cm
 
 dict_plt_sst = {'vmin':-2,'vmax':32,'colorbar':cm.gist_ncar}
 dict_plt_sss = {'vmin':23,'vmax':40,'colorbar':cm.jet}
+dict_plt_ssh = {'vmin':-2,'vmax':1,'colorbar':cm.jet}
+dict_plt_bathy = {'vmin':0,'vmax':5000,'colorbar':cm.jet}
 
 aste = regional_llc()
 aste.plot_map_field('/Users/raphael/WORK/ASTE/test_results','T',dict_plt_sst,level=0,timestep=2307,precision='single')
 aste.plot_map_field('/Users/raphael/WORK/ASTE/test_results','S',dict_plt_sss,level=0,timestep=2307,precision='single')
+aste.plot_map_field('/Users/raphael/WORK/ASTE/test_results','Eta',dict_plt_ssh,timestep=2307,precision='single')
+
+# bug plot
+aste.plot_map_field('/Users/raphael/WORK/ASTE/test_results/','',dict_plt_sst,level=0,filealt='../input_files/WOA09v2_T_llc270_JAN.bin',precision='double')
+
+# on llc global grid
+#aste.plot_map_field('/Users/raphael/WORK/ASTE/test_results/','',dict_plt_bathy,filealt='../input_files/bathy_fill9iU42Ef_noStLA_v1.bin',precision='single')
+
--- post-processing/recombination/llc/lib_regional_llc.py
+++ post-processing/recombination/llc/lib_regional_llc.py
@@ -18,12 +18,15 @@ class regional_llc():
 		else:
 			pass
 
-	def read_2d_field(self,dirin,variable,timestep=None,precision='single'):
+	def read_2d_field(self,dirin,variable,timestep=None,precision='single',filealt=None):
 		''' read 2d field from .data file '''
 		if timestep is None:
 			filein = dirin + '/' + variable + '.data'
 		else:
 			filein = dirin + '/' + variable + '.' + str(timestep).zfill(10) + '.data'
+		# overide filein if filealt exists (usefull for input binaries,...)
+		if filealt is not None:
+			filein = dirin + filealt
 		# load data
 		if precision == 'single':
 			data_raw = np.fromfile(filein,'>f')
@@ -42,12 +45,14 @@ class regional_llc():
 
 		return [data_f1,data_f2,data_f3,data_f4,data_f5]
 
-	def read_3d_field(self,dirin,variable,timestep=None,precision='single'):
+	def read_3d_field(self,dirin,variable,timestep=None,precision='single',filealt=None):
 		''' read 3d field from .data file '''
 		if timestep is None:
 			filein = dirin + '/' + variable + '.data'
 		else:
 			filein = dirin + '/' + variable + '.' + str(timestep).zfill(10) + '.data'
+		if filealt is not None:
+			filein = dirin + filealt
 		# load data
 		if precision == 'single':
 			data_raw = np.fromfile(filein,'>f')
@@ -66,17 +71,48 @@ class regional_llc():
 
 		return [data_f1,data_f2,data_f3,data_f4,data_f5]
 
-	def plot_map_field(self,dirin,variable,dict_plt,level=None,timestep=None,precision='single'):
+	def read_4d_field(self,dirin,variable,ntimes,timestep=None,precision='single',filealt=None):
+		''' read 4d field from .data file '''
+		if timestep is None:
+			filein = dirin + '/' + variable + '.data'
+		else:
+			filein = dirin + '/' + variable + '.' + str(timestep).zfill(10) + '.data'
+		if filealt is not None:
+			filein = dirin + filealt
+		# load data
+		if precision == 'single':
+			data_raw = np.fromfile(filein,'>f')
+		elif precision == 'double':
+			data_raw = np.fromfile(filein,'>d')
+
+		# reshape on dims
+		data_compact = np.reshape(data_raw,[ntimes,self.nz,self.ny_total,self.nx])
+		del data_raw
+		# split in faces
+		data_f1 = data_compact[:,:,:self.ny_end[0],:]
+		data_f2 = data_compact[:,:,self.ny_end[0]:self.ny_end[1],:]
+		data_f3 = data_compact[:,:,self.ny_end[1]:self.ny_end[2],:]
+		data_f4 = np.reshape(data_compact[:,:,self.ny_end[2]:self.ny_end[3],:],\
+		[ntimes,self.nz,self.nx_fn[3],self.ny_fn[3]])
+		data_f5 = np.reshape(data_compact[:,:,self.ny_end[3]:self.ny_end[4],:],\
+		[ntimes,self.nz,self.nx_fn[4],self.ny_fn[4]])
+
+		return [data_f1,data_f2,data_f3,data_f4,data_f5]
+
+	def plot_map_field(self,dirin,variable,dict_plt,frame=None,level=None,timestep=None,precision='single',filealt=None):
 		''' plot map of 2d or 3d field '''
 		XCs = self.read_2d_field(dirin,'XC')
 		YCs = self.read_2d_field(dirin,'YC')
-		if level is None:
-			FIELDs = self.read_2d_field(dirin,variable,timestep=timestep,precision=precision)
-		else:
-			tmp = self.read_3d_field(dirin,variable,timestep=timestep,precision=precision)
-			FIELDs = []
-			for kk in np.arange(5):
-				FIELDs.append(tmp[kk][level,:,:])
+		if frame is None:
+			if level is None:
+				FIELDs = self.read_2d_field(dirin,variable,timestep=timestep,\
+				precision=precision,filealt=filealt)
+			else:
+				tmp = self.read_3d_field(dirin,variable,timestep=timestep,\
+				precision=precision,filealt=filealt)
+				FIELDs = []
+				for kk in np.arange(5):
+					FIELDs.append(tmp[kk][level,:,:])
 
 		# do the actual plot
 		plt.figure(figsize=[10,8])
@@ -95,6 +131,7 @@ class regional_llc():
 			xx5,yy5 = m(XCs[4],YCs[4])
 			m.contourf(xx5,yy5,np.ma.masked_values(FIELDs[4][:,:],0),ncontours,vmin=dict_plt['vmin'],\
 			vmax=dict_plt['vmax'],cmap=dict_plt['colorbar'])
+			plt.colorbar()
 			m.fillcontinents(color='grey',lake_color='white')
 			m.drawcoastlines()
 			plt.show()
