2d90156e745f29043096d8f76ba69d7e5a6be52d
Raphael Dussin <raphael.dussin@gmail.com>
2018-06-11 15:13:33 -0400

    cleanup binary files

--- pre-processing/obc/create_obc_bling_ASTE.py
+++ pre-processing/obc/create_obc_bling_ASTE.py
@@ -4,10 +4,15 @@ import xarray as xr
 datasetdir='../regridding/'
 nameroot='bling_ASTE_FACET<FACET>.nc'
 
+list_facets = ['1','3','4','5']
+list_bgc_variables = ['alk','dic','o2','no3','po4','fed','n_org','p_org']
 list_dataset = []
-for facet in ['1','3','4','5']:
+
+for facet in list_facets:
 	list_dataset.append(xr.open_dataset(datasetdir + nameroot.replace('<FACET>',facet)))
 
 aste_obc = create_obc('/Users/raphael/WORK/MITgcm_runs/ASTE-TEST01/input_BE2_dthetadr/data.obcs')
-aste_obc.create_obc_regional_llc('alk','south',list_dataset,'alk_south_bling.bin')
-aste_obc.create_obc_regional_llc('alk','east',list_dataset,'alk_east_bling.bin')
+
+for var in list_bgc_variables:
+	aste_obc.create_obc_regional_llc(var,'south',list_dataset,var + '_south_bling.bin')
+	aste_obc.create_obc_regional_llc(var,'east',list_dataset,var + '_east_bling.bin')
