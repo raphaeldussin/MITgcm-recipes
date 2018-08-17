08e15babf9ffee157f3c829dcd5708967a6f0116
Raphael Dussin <rdussin@artemis.ldeo.columbia.edu>
2018-08-17 13:50:04 -0400

    cleanup

--- pre-processing/regridding/interp_BLING_ASTE.py
+++ pre-processing/regridding/interp_BLING_ASTE.py
@@ -1,37 +1,44 @@
 import xarray as xr
 from lib_regrid2MITgcm import *
 
-bling_global = xr.open_dataset('/Volumes/L2/ASTE/global_BLINGv2/C1p5.control.3201-3300.ocean_bling_avg.nc')
+# laptop
+dirbling = '/Volumes/L2/ASTE/global_BLINGv2/'
+dirgrid  = '/Users/raphael/TOOLS/MITgcm-recipes/pre-processing/grid/'
+dirout   = '/Volumes/L2/ASTE/global_BLINGv2_regridded_ASTE/'
+
+# artemis
+dirbling = '/local/data/artemis/workspace/rdussin/ASTE/global_BLINGv2/'
+dirgrid  = '/local/data/artemis/workspace/rdussin/ASTE/GRID/'
+dirout   = '/local/data/artemis/workspace/rdussin/ASTE/global_BLINGv2_regridded_ASTE/'
+
+bling_global = xr.open_dataset(dirbling + 'C1p5.control.3201-3300.ocean_bling_avg.nc')
 variables2regrid = ['alk','dic','o2','no3','po4','fed','n_org','p_org']
-dirout='/Volumes/L2/ASTE/global_BLINGv2_regridded_ASTE/'
 
 #--------------- FACET 1 ---------------------
-aste_grid1 = xr.open_dataset('/Users/raphael/TOOLS/MITgcm-recipes/pre-processing/grid/ASTE_FACET1.nc')
+aste_grid1 = xr.open_dataset(dirgrid + 'ASTE_FACET1.nc')
 Regrid2ASTE1 = Regrid2MITgcm(aste_grid1)
-interpolated1 = Regrid2ASTE1.regrid(bling_global,variables2regrid,'geolon_t','geolat_t',method='bilinear',blend_missing=True,periodicity=0,periodic=True)
+interpolated1 = Regrid2ASTE1.regrid(bling_global, variables2regrid, 'geolon_t', 'geolat_t',
+                                    method='bilinear', blend_missing=False, periodicity=0, periodic=True)
 interpolated1.to_netcdf(dirout + 'bling_ASTE_FACET1.nc')
 
-#--------------- FACET 2 ---------------------
-#aste_grid2 = xr.open_dataset('/Users/raphael/TOOLS/MITgcm-recipes/pre-processing/grid/ASTE_FACET2.nc')
-#Regrid2ASTE2 = Regrid2MITgcm(aste_grid2)
-#interpolated2 = Regrid2ASTE2.regrid(bling_global,variables2regrid,'geolon_t','geolat_t',method='bilinear',blend_missing=True,periodicity=0,periodic=True)
-#interpolated2.to_netcdf(dirout + 'bling_ASTE_FACET2.nc')
-
 #--------------- FACET 3 ---------------------
-aste_grid3 = xr.open_dataset('/Users/raphael/TOOLS/MITgcm-recipes/pre-processing/grid/ASTE_FACET3.nc')
+aste_grid3 = xr.open_dataset(dirgrid + 'ASTE_FACET3.nc')
 Regrid2ASTE3 = Regrid2MITgcm(aste_grid3)
-interpolated3 = Regrid2ASTE3.regrid(bling_global,variables2regrid,'geolon_t','geolat_t',method='bilinear',blend_missing=True,periodicity=0,periodic=False)
+interpolated3 = Regrid2ASTE3.regrid(bling_global, variables2regrid, 'geolon_t', 'geolat_t',
+                                    method='bilinear', blend_missing=True, periodicity=0, periodic=False)
 interpolated3.to_netcdf(dirout + 'bling_ASTE_FACET3.nc')
 
 #--------------- FACET 4 ---------------------
-aste_grid4 = xr.open_dataset('/Users/raphael/TOOLS/MITgcm-recipes/pre-processing/grid/ASTE_FACET4.nc')
+aste_grid4 = xr.open_dataset(dirgrid + 'ASTE_FACET4.nc')
 Regrid2ASTE4 = Regrid2MITgcm(aste_grid4)
-interpolated4 = Regrid2ASTE4.regrid(bling_global,variables2regrid,'geolon_t','geolat_t',method='bilinear',blend_missing=True,periodicity=0,periodic=True)
+interpolated4 = Regrid2ASTE4.regrid(bling_global, variables2regrid, 'geolon_t', 'geolat_t',
+                                    method='bilinear', blend_missing=False, periodicity=0, periodic=True)
 interpolated4.to_netcdf(dirout + 'bling_ASTE_FACET4.nc')
 
 #--------------- FACET 5 ---------------------
-aste_grid5 = xr.open_dataset('/Users/raphael/TOOLS/MITgcm-recipes/pre-processing/grid/ASTE_FACET5.nc')
+aste_grid5 = xr.open_dataset(dirgrid + 'ASTE_FACET5.nc')
 Regrid2ASTE5 = Regrid2MITgcm(aste_grid5)
-interpolated5 = Regrid2ASTE5.regrid(bling_global,variables2regrid,'geolon_t','geolat_t',method='bilinear',blend_missing=True,periodicity=0,periodic=True)
+interpolated5 = Regrid2ASTE5.regrid(bling_global, variables2regrid, 'geolon_t', 'geolat_t',
+                                    method='bilinear', blend_missing=False, periodicity=0, periodic=True)
 interpolated5.to_netcdf(dirout + 'bling_ASTE_FACET5.nc')
 
--- pre-processing/regridding/lib_regrid2MITgcm.py
+++ pre-processing/regridding/lib_regrid2MITgcm.py
@@ -8,23 +8,25 @@ import mod_akima_1d
 
 class Regrid2MITgcm():
 
-	def __init__(self,model_grid):
+	def __init__(self, model_grid):
 		''' init the regridding object with model grid
 		type(model_grid) = xarray '''
 		self.model_grid = model_grid
 		self.spval = -9999.
 		return None
 
-	def regrid(self,dataset,list_variables,lonvarname,latvarname,method='bilinear',blend_missing=True,periodicity=0,target_point='center',mask_output=True,periodic=True):
+	def regrid(self, dataset, list_variables, lonvarname, latvarname, method='bilinear',
+                   blend_missing=True, periodicity=0, target_point='center', mask_output=True,
+                   periodic=True, reuse_weights=False):
 		''' regrid the input xarray dataset
 		list_vars = list of strings
 		'''
 		# add periodic attr
 		# xESMF wants lon/lat to be the names of coords for interpolation
-		inputds = dataset.rename({lonvarname: 'lon', latvarname: 'lat'})#, inplace=True)
+		inputds = dataset.rename({lonvarname: 'lon', latvarname: 'lat'})
 		# define an output dataset from grid with correct destination points
 		if target_point == 'center':
-			target_grid = self.model_grid.rename({'XC': 'lon', 'YC': 'lat'})#, inplace=True)
+			target_grid = self.model_grid.rename({'XC': 'lon', 'YC': 'lat'})
 		else:
 			pass # TO DO
 
@@ -32,31 +34,28 @@ class Regrid2MITgcm():
 			# build land sea mask
 			maskvar = self._mask_from_missing_value(inputds[variable])
 			# extrapolate onto land for coastal values
-			inputds[variable] = self._drown_field(inputds[variable],maskvar,periodicity=periodicity)
+			inputds[variable] = self._drown_field(inputds[variable], maskvar, periodicity=periodicity)
 
-			#plt.figure()
-			#inputds[variable][0,0,:,:].plot() ; plt.show()
-
-		hremapped = self._perform_interpolation(inputds,target_grid,list_variables,method=method,blend_missing=blend_missing,periodic=periodic)
+		hremapped = self._perform_interpolation(inputds, target_grid, list_variables, method=method,
+                                                        blend_missing=blend_missing, periodic=periodic,
+                                                        reuse_weights=reuse_weights)
 
 		# vertical interpolation
-		outputds = self. _vertical_interpolation(inputds,hremapped,target_grid,list_variables)
-
-		# TO DO
+		outputds = self. _vertical_interpolation(inputds, hremapped, target_grid, list_variables)
 
 		if mask_output:
-			outputds = self._mask_output(outputds,target_grid,list_variables)
+			outputds = self._mask_output(outputds, target_grid, list_variables)
 
 		return outputds
 
 
-	def _mask_from_missing_value(self,dataarray):
+	def _mask_from_missing_value(self, dataarray):
 		''' create binary 1/0 mask for masked values in dataarray '''
 		mask = np.ones(dataarray.shape)
 		mask[np.isnan(dataarray.values)] = 0
 		return mask
 
-	def _drown_field(self,dataarray,mask,periodicity=0):
+	def _drown_field(self, dataarray, mask, periodicity=0):
 		''' periodicity : 0 = periodic, 2 = 2 points overlap,...
 		periodicity : -1 =non-periodic
 
@@ -76,17 +75,18 @@ class Regrid2MITgcm():
 					masklevel = mask[k0,k1,::].squeeze()
 					# the NaNs in xarray don't make fortran happy
 					tmpin[np.isnan(tmpin)] = self.spval
-					tmpout = mod_drown_sosie.mod_drown.drown(periodicity,tmpin.transpose(),masklevel.transpose(),\
+					tmpout = mod_drown_sosie.mod_drown.drown(periodicity,tmpin.transpose(),
+                                                                                 masklevel.transpose(),\
 					                                         nb_inc=100,nb_smooth=20)
 					data_drowned[k0,k1,:,:] = tmpout.transpose()
 		else:
 			pass # TO DO
 
-		dataarray_out = xr.DataArray(data_drowned,coords=dataarray.coords,dims=dataarray.dims)
+		dataarray_out = xr.DataArray(data_drowned, coords=dataarray.coords, dims=dataarray.dims)
 
 		return dataarray_out
 
-	def _blend(self,da1,da2,missing):
+	def _blend(self, da1, da2, missing):
 		tmp1 = da1.values
 		tmp2 = da2.values
 		mask = np.ones(tmp1.shape)
@@ -95,16 +95,16 @@ class Regrid2MITgcm():
 		da3 = xr.DataArray(tmp3,dims=da1.dims)
 		return da3
 
-	def _perform_interpolation(self,input_dataset,target_grid,list_variables,method='bilinear',blend_missing=True,periodic=True):
+	def _perform_interpolation(self, input_dataset, target_grid, list_variables, method='bilinear',
+                                   blend_missing=True, periodic=True, reuse_weights=False):
 
 		# create interpolators
-		# not sure if I want to re-use the weights (source of potential mistakes)
-		# add periodic option
-		regridder = xe.Regridder(input_dataset, target_grid, method, periodic=periodic) #,reuse_weights=True)
+		regridder = xe.Regridder(input_dataset, target_grid, method, periodic=periodic, 
+                                         reuse_weights=reuse_weights)
 		if blend_missing:
-			backup_regridder = xe.Regridder(input_dataset, target_grid, 'nearest_s2d',periodic=periodic) #,reuse_weights=True)
+			backup_regridder = xe.Regridder(input_dataset, target_grid, 'nearest_s2d',
+                                                        periodic=periodic, reuse_weights=reuse_weights)
 
-		#hremapped = target_grid.copy()
 		hremapped = xr.Dataset()
 		hremapped.update({'XC':target_grid['lon']})
 		hremapped.update({'YC':target_grid['lat']})
@@ -117,11 +117,10 @@ class Regrid2MITgcm():
 
 			hremapped.update({variable:out_da})
 
-		print(hremapped)
 		return hremapped
 
 
-	def _vertical_interpolation(self,inputds,hremapped,target_grid,list_variables):
+	def _vertical_interpolation(self, inputds, hremapped, target_grid, list_variables):
 
 		outputds = target_grid.copy()
 		for variable in list_variables:
@@ -136,7 +135,8 @@ class Regrid2MITgcm():
 				ny = len(hremapped[variable].coords[hremapped[variable].dims[2]])
 				nx = len(hremapped[variable].coords[hremapped[variable].dims[3]])
 
-				dest_dims = (hremapped[variable].dims[0],'z',hremapped[variable].dims[2],hremapped[variable].dims[3])
+				dest_dims = (hremapped[variable].dims[0],'z', hremapped[variable].dims[2],
+                                             hremapped[variable].dims[3])
 			#elif len(hremapped[variable].dims) == 3:
 			#	vert_axis = 0
 			#	nt = 1
@@ -157,21 +157,23 @@ class Regrid2MITgcm():
 			#print(znew)
 			#data_regridded = xr.DataArray(
 
-			remapped = xr.DataArray(np.zeros((nt,nz,ny,nx)),dims=dest_dims)
+			remapped = xr.DataArray(np.zeros((nt,nz,ny,nx)), dims=dest_dims)
 
 			if ndims_loop == 1:
 				for k0 in np.arange(len(hremapped[variable].coords[hremapped[variable].dims[0]])):
-					remapped[k0,:] = mod_akima_1d.mod_akima_1d.vertical_interpolation(zold,hremapped[variable][k0,:].values,znew)
-					#outputds[variable][k0,:] = mod_akima_1d.mod_akima_1d.vertical_interpolation(zold,hremapped[variable][k0,:].values,znew)
+					remapped[k0,:] = mod_akima_1d.mod_akima_1d.vertical_interpolation(zold,
+                                                         hremapped[variable][k0,:].values,znew)
+					#outputds[variable][k0,:] = mod_akima_1d.mod_akima_1d.vertical_
+                                        #interpolation(zold,hremapped[variable][k0,:].values,znew)
 					outputds.update({variable:remapped})
 			#elif ndims_loop == 0:
-			#	outputds[variable][:] = mod_akima_1d.mod_akima_1d.vertical_interpolation(zold,hremapped[variable][:].values,znew)
-
+			#	outputds[variable][:] = mod_akima_1d.mod_akima_1d.
+                        #vertical_interpolation(zold,hremapped[variable][:].values,znew)
 
 		return outputds
 
 
-	def _mask_output(self,outputds,target_grid,list_variables):
+	def _mask_output(self, outputds, target_grid, list_variables):
 		''' mask the interpolated field with land sea mask from target grid '''
 		for variable in list_variables:
 			ndims_mask = len(target_grid.lsm.dims)
