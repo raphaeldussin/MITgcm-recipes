f5704ce991adf3953927afb7ec69669a4444b457
Raphael Dussin <raphael.dussin@gmail.com>
2018-12-19 17:46:34 -0500

    start working on regridding ctrl

---
+++ pre-processing/forcing/ctrl2atmo/correct_atmo_forcing.py
@@ -0,0 +1,20 @@
+from lib_correct_atmo_forcing import *
+import pandas as pd
+
+jragrid='./jra_grid.nc'
+
+dirin='/t0/scratch/raphael/ASTE/config_AnTNguyen.v2/ADXXfiles/'
+astegrid='/t0/scratch/raphael/ASTE/tmpdir_ASTE-Phy-REF01/'
+
+lon_atm, lat_atm = read_forcing_grid(jragrid, lonvar='longitude', latvar='latitude')
+data_ctrl, grid_ctrl = read_ctrl_file(dirin + 'xx_aqh', 55, 419, astegrid)
+
+data = regrid_to_forcing(lon_atm, lat_atm, grid_ctrl, data_ctrl)
+
+ds = xr.Dataset({'temperature': (['time', 'lat', 'lon'], data)},
+                 coords={'lon': (['lon'], lon_atm),
+                         'lat': (['lat'], lat_atm),
+                         'time': pd.date_range('2002-01-01', periods=419),
+                         'reference_time': pd.Timestamp('2002-01-01')})
+
+ds.to_netcdf('test_spechum.nc')
---
+++ pre-processing/forcing/ctrl2atmo/lib_correct_atmo_forcing.py
@@ -0,0 +1,70 @@
+import xarray as xr
+import xmitgcm 
+import pyresample as _pyresample
+import numpy as np
+
+def read_forcing_grid(filein, lonvar='lon', latvar='lat'):
+    ''' read coordinates of forcing grid '''
+    ds = xr.open_dataset(filein)
+    lon = ds[lonvar].values
+    lat = ds[latvar].values
+
+    return lon, lat
+
+def read_ctrl_file(varname, optim_cycle, ntimes, grid_dir): 
+    ''' read the content of the XX file '''
+    astemd = xmitgcm.utils.get_extra_metadata(domain='aste',nx=270)
+
+    tmp = xmitgcm.utils.read_mds(varname, iternum=optim_cycle, use_mmap=False,
+                                  endian='>', dtype=np.dtype('f'), use_dask=True, 
+                                  extra_metadata=astemd, chunks='2D', llc=True, legacy=True,
+                                  shape=(ntimes,astemd['ny'],astemd['nx']))
+    
+    grid = xmitgcm.open_mdsdataset(grid_dir, prefix=['XC','YC'], geometry='llc', 
+                                   extra_metadata=astemd, nx=astemd['nx'])
+
+    data = xr.DataArray(tmp[list(tmp.keys())[0]], dims=['time', 'face', 'j', 'i'])
+
+    return data, grid
+
+def regrid_to_forcing(lon_atm, lat_atm, grid_ctrl, data_ctrl):
+    ''' regrid from model grid to atmospheric grid '''
+
+    nt = data_ctrl.shape[0]
+    nyout = lat_atm.shape[0]
+    nxout = lon_atm.shape[0]
+
+    # input grid
+    lon_ctrl = grid_ctrl['XC'].values.ravel()
+    lat_ctrl = grid_ctrl['YC'].values.ravel()
+    # lon_ctrl = np.mod(lon_ctrl+360,360) does not work, see below
+
+    grid_src = _pyresample.geometry.SwathDefinition(lons=lon_ctrl, 
+                                                    lats=lat_ctrl)
+
+    # output grid (but shifted to -180/180 to fit ASTE)
+    # Note: for some unexplained reason, moving ASTE to 0-360 does not do the job
+    # and has all values between 180/360 not computed
+    lons_atm, lats_atm = np.meshgrid(lon_atm, lat_atm)
+    grid_target = _pyresample.geometry.GridDefinition(lons=lons_atm-180.,
+                                                      lats=lats_atm)
+
+    # we need to be able to reset the data on the 0-360 forcing grid
+    ind_greenwich= (np.abs(lon_atm-180.)).argmin()
+
+    # let's do that regridding!
+    data = np.empty((nt, nyout, nxout))
+    for kt in np.arange(nt): 
+        print('working on step' + str(kt))
+        # get current frame
+        data_step = data_ctrl.isel(time=kt).values.ravel()
+
+        # interpolate
+        data_interp = _pyresample.kd_tree.resample_nearest(grid_src, data_step,
+                                                           grid_target,
+                                                           radius_of_influence=50000,
+                                                           fill_value=0)
+        # roll back from -180/180 to 0/360
+        data[kt,:,:] = np.roll(data_interp, -ind_greenwich)
+
+    return data
