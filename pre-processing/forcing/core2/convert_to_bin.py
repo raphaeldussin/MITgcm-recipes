3ca386a5cd83e02795eab272cfaa4209604492e1
Raphael Dussin <raphael.dussin@gmail.com>
2019-02-05 20:18:37 -0500

    work on CORE2

--- pre-processing/forcing/core2/convert_to_bin.py
+++ pre-processing/forcing/core2/convert_to_bin.py
@@ -1,6 +1,7 @@
 import xarray as xr
 import numpy as np
 import sys
+import calendar
 
 def write_to_binary(ds, variable, fileout, precision='single'):
     ''' write variable from dataset ds to fileout with precision '''
@@ -21,26 +22,65 @@ def write_to_binary(ds, variable, fileout, precision='single'):
     fid.close()
     return None
 
-core_dir='/local/data/artemis/workspace/rdussin/CORE2/by_year/'
+core_dir='/local/data/artemis/workspace/rdussin/CORE2-GFDL/yearly/'
 
-listvar = ['Pair','Qair','Tair','Uwind','Vwind','lwrad','rain','swrad']
-for year in np.arange(1948,2007+1):
-    print('working on year', year)
+# 6-hourly
+listvar = ['Q_10_MOD','T_10_MOD','U_10_MOD','V_10_MOD']
+
+for year in np.arange(1948,2009+1):
+    print('6-hourly, working on year', year)
+    filelist=[]
+    for var in listvar:
+        filelist.append(core_dir + var + '_CORE2_y' + str(year) + '.nc')
+
+    dsyear = xr.open_mfdataset(filelist, decode_times=False)
+
+    # add a second january 1st if leap year
+    if calendar.isleap(year):
+        ds = xr.concat([dsyear.isel(TIME=range(4)), dsyear], dim='TIME')
+    else:
+        ds = dsyear
+
     for var in listvar:
         print('working on variable', var)
-        filein  = core_dir + var + '_CORE2_y' + str(year) + '.nc'
         fileout = core_dir + 'binaries/' + var + '_CORE2_y' + str(year)
-
-        if var == 'lwrad':
-            var = 'lwrad_down'
-
-        ds = xr.open_dataset(filein, decode_times=False)
-
-        # unit conversion
-        if var == 'Tair':
-            ds['Tair'] = 273.15 + ds['Tair'] # from degC to K
-
-        if var == 'rain':
-            ds['rain'] = ds['rain'] / 1000. # [kg.m-2.s-1] / [kg/m3] = [m/s]
-
         write_to_binary(ds, var, fileout, precision='single')
+
+# daily
+listvar = ['LWDN_MOD','SWDN_MOD']
+
+for year in np.arange(1948,2009+1):
+    print('daily, working on year', year)
+    filelist=[]
+    for var in listvar:
+        filelist.append(core_dir + var + '_CORE2_y' + str(year) + '.nc')
+
+    dsyear = xr.open_mfdataset(filelist, decode_times=False)
+
+    # add a second january 1st if leap year
+    if calendar.isleap(year):
+        ds = xr.concat([dsyear.isel(TIME=0), dsyear], dim='TIME')
+    else:
+        ds = dsyear
+
+    for var in listvar:
+        print('working on variable', var)
+        fileout = core_dir + 'binaries/' + var + '_CORE2_y' + str(year)
+        write_to_binary(ds, var, fileout, precision='single')
+
+# monthly
+listvar = ['RAIN','SNOW']
+
+for year in np.arange(1948,2009+1):
+    print('monthly, working on year', year)
+    filelist=[]
+    for var in listvar:
+        filelist.append(core_dir + var + '_CORE2_y' + str(year) + '.nc')
+
+    dsyear = xr.open_mfdataset(filelist, decode_times=False)
+    # total precip + unit conversion
+    dsyear['PRECIP'] = (dsyear['RAIN'] + dsyear['SNOW']) /1000. # [kg.m-2.s-1] / [kg/m3] = [m/s]
+
+    fileout = core_dir + 'binaries/' + 'PRECIP' + '_CORE2_y' + str(year)
+    write_to_binary(dsyear, 'PRECIP', fileout, precision='single')
+
--- pre-processing/forcing/core2/gen_nml_entry.py
+++ pre-processing/forcing/core2/gen_nml_entry.py
@@ -4,12 +4,12 @@ import numpy as np
 
 def write_grid_info(ds, varname, fileout):
     ''' write formatted data for data.exf '''
-    lon0 = ds.lon.values.min()
-    lon_inc = ds.lon.values[1] - ds.lon.values[0]
-    lat0 = ds.lat.values.min()
-    lat_inc = ds.lat.values[1:] - ds.lat.values[:-1]
-    nlon = len(ds.lon.values)
-    nlat = len(ds.lat.values)
+    lon0 = ds.LON.values.min()
+    lon_inc = ds.LON.values[1] - ds.LON.values[0]
+    lat0 = ds.LAT.values.min()
+    lat_inc = ds.LAT.values[1:] - ds.LAT.values[:-1]
+    nlon = len(ds.LON.values)
+    nlat = len(ds.LAT.values)
 
     line1 = ' ' + varname + '_lon0       = %.3fD0,' % (lon0) 
     line2 = ' ' + varname + '_lon_inc    = %.3fD0,' % (lon_inc)
@@ -35,28 +35,28 @@ def write_grid_info(ds, varname, fileout):
     fid.close()
     return None
 
-dir_core='/local/data/artemis/workspace/rdussin/CORE2/'
+dir_core='/local/data/artemis/workspace/rdussin/CORE2-GFDL/'
 
 fileout='data.exf_CORE2'
 
-ds = xr.open_mfdataset(dir_core + 'rain.1948-2007.nc', decode_times=False)
+ds = xr.open_mfdataset(dir_core + 'ncar_precip.1948-2009.23OCT2012.nc', decode_times=False)
 write_grid_info(ds, 'precip', fileout)
 
-ds = xr.open_mfdataset(dir_core + 'Tair.1948-2007.nc', decode_times=False)
+ds = xr.open_mfdataset(dir_core + 't_10.1948-2009.23OCT2012.nc', decode_times=False)
 write_grid_info(ds, 'atemp', fileout)
 
-ds = xr.open_mfdataset(dir_core + 'Qair.1948-2007.nc', decode_times=False)
+ds = xr.open_mfdataset(dir_core + 'q_10.1948-2009.23OCT2012.nc', decode_times=False)
 write_grid_info(ds, 'aqh', fileout)
 
-ds = xr.open_mfdataset(dir_core + 'swrad.1948-2007.nc', decode_times=False)
+ds = xr.open_mfdataset(dir_core + 'ncar_rad.1948-2009.23OCT2012.nc', decode_times=False)
 write_grid_info(ds, 'swdown', fileout)
 
-ds = xr.open_mfdataset(dir_core + 'lwrad.1948-2007.nc', decode_times=False)
+ds = xr.open_mfdataset(dir_core + 'ncar_rad.1948-2009.23OCT2012.nc', decode_times=False)
 write_grid_info(ds, 'lwdown', fileout)
 
-ds = xr.open_mfdataset(dir_core + 'Uwind.1948-2007.nc', decode_times=False)
+ds = xr.open_mfdataset(dir_core + 'u_10.1948-2009.23OCT2012.nc', decode_times=False)
 write_grid_info(ds, 'uwind', fileout)
 
-ds = xr.open_mfdataset(dir_core + 'Vwind.1948-2007.nc', decode_times=False)
+ds = xr.open_mfdataset(dir_core + 'v_10.1948-2009.23OCT2012.nc', decode_times=False)
 write_grid_info(ds, 'vwind', fileout)
 
